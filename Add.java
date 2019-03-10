714ade93ff145234de78f305cb0fc10b74aaafe6
sowmyakasarla9 <sowmyareddykasarla99@gmail.com>
2019-03-10 20:25:28 +0530

    added new file

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Add
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a,b,c;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+	System.out.println("sum ="+c);
+	}
+}
+		
---
+++ FirstDemo.java
@@ -0,0 +1,8 @@
+public class FirstDemo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Task 1");
+		System.out.println("Simple Java Project");
+	}
+}
